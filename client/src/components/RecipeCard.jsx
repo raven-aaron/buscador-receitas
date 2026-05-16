37d6da778021d58d97adc3636db349d6a22f31a5
Raven Aaron <raven.aaron503@gmail.com>
2026-05-16 15:42:59 -0300

    feat: add feature 'Missing ingredients'

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -11,6 +11,15 @@ export default function RecipeCard({ receita }) {
       <h3 className="titulo-receita">
         {receita.title}
       </h3>
+      
+      <div className="ingredientes-faltantes">
+        <p className="titulo-ingredientes">Missing ingredients:</p>
+        <ul>
+          {receita.missedIngredients.map((ingrediente) => (
+            <li key={ingrediente.id}>{ingrediente.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
